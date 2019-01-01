3829bc3fc83ea8f360f356cfdca1331f42277959
hannaertel <hannaertel@yahoo.com>
2019-01-01 03:39:21 +0000

    Automatically backed up by Learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -3,7 +3,18 @@ rainbow_colors = [
   "default",
   "light_cyan"
 ]
-
+def change_rainbow_colorss
+  rainbow_colors = [
+    "yellow",
+    "default",
+    "light_cyan"]
+    rainbow_colors[0] == "red"
+    rainbow_colors[1] == "light_red"
+    rainbow_colors[2] == "light_yellow"
+    puts rainbow_colors
+  end
+  change_rainbow_colorss
+  
 def change_rainbow_colors
   
   rainbow_colors = [
