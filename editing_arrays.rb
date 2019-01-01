ea6a8e3793d36b1375bba53f0c2766a0ad79a515
hannaertel <hannaertel@yahoo.com>
2019-01-01 03:49:44 +0000

    Automatically backed up by Learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -12,6 +12,7 @@ def change_rainbow_colorss
     rainbow_colors[1] = "light_red"
     rainbow_colors[2] = "light_yellow"
     puts rainbow_colors
+    rainbow_colors.push["green", "blue"]
   end
   change_rainbow_colorss
   
@@ -28,6 +29,7 @@ def change_rainbow_colors
   puts rainbow_colors 
   
  end
+ 
  change_rainbow_colors
  rainbow_colors.push("green", "blue")
  
